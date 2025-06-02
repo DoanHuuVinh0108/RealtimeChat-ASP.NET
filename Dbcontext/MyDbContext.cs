4c428b3e3f36b4b5fc494d5d6a61b21a72f0eef0
vinhb2014633@student.ctu.edu.vn <vinhb2014633@student.ctu.edu.vn>
2025-06-02 15:23:52 +0700

    Add project files.

---
+++ Dbcontext/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Chat_1_to_1.Models;
+namespace Chat_1_to_1.Dbcontext
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+       
+    }
+}
